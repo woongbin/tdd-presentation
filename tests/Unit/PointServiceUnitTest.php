0b20b77f9183a473055fb5ef3d453ec0e759ab9c
sirwoongke <fzwoongbin@naver.com>
2022-02-20 20:36:37 +0900

    2-C 테스트 코드 작성 red 단계

--- tests/Unit/PointServiceUnitTest.php
+++ tests/Unit/PointServiceUnitTest.php
@@ -20,7 +20,7 @@ class PointServiceUnitTest extends TestCase
         $this->pointService = app(PointService::class);
     }
 
-    private function getProduct($price = 0, $pointRate = 0.05): Product
+    private function getProduct($price = 0, $pointRate = 0.0): Product
     {
         $price = $price > 0 ? $price : $this->faker->numberBetween(1000, 100000);
 
@@ -74,5 +74,24 @@ class PointServiceUnitTest extends TestCase
         $result = $this->pointService->getPoint($product);
 
         $this->assertEquals($expectResult, $result);
+    }
+
+    /** @test */
+    public function 상품에서_제공하는_포인트_적립률로_포인트를_계산한다(): void
+    {
+        $testParameters = [];
+        $testParameters[] = ['price' => 10000, 'pointRate' => 0.1, 'result' => 1000];
+        $testParameters[] = ['price' => 10000, 'pointRate' => 0.05, 'result' => 500];
+        $testParameters[] = ['price' => 10000, 'pointRate' => 0.07, 'result' => 700];
+        $testParameters[] = ['price' => 8300, 'pointRate' => 0.1, 'result' => 830];
+        $testParameters[] = ['price' => 8375, 'pointRate' => 0.03, 'result' => 251];
+
+        foreach ($testParameters as $parameter) {
+            $product = $this->getProduct($parameter['price'], $parameter['pointRate']);
+
+            $result = $this->pointService->getPoint($product);
+
+            $this->assertEquals($parameter['result'], $result);
+        }
     }
 }
