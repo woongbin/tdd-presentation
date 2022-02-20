0a384a4be87f842cf76e12274b2d9a77c44e589f
sirwoongke <fzwoongbin@naver.com>
2022-02-20 15:55:03 +0900

    1-C 테스트 코드 작성 red 단계

--- tests/Unit/PointServiceUnitTest.php
+++ tests/Unit/PointServiceUnitTest.php
@@ -17,8 +17,14 @@ class PointServiceUnitTest extends TestCase
         $this->pointService = app(PointService::class);
     }
 
-    private function getProduct(): Product
+    private function getProduct($price = 0): Product
     {
+        if ($price > 0) {
+            return Product::factory()->state([
+                'price' => $price
+            ])->make();
+        }
+
         return Product::factory()->make();
     }
 
@@ -41,5 +47,33 @@ class PointServiceUnitTest extends TestCase
         $result = $this->pointService->getPoint($product);
 
         $this->assertIsInt($result);
+    }
+
+    /** @test */
+    public function 상품_가격의_5퍼센트가_정수인_경우_그_값을_반환한다(): void
+    {
+        $price = 10000;
+        //가격이 10,000원일 경우 5%인 500을 반환
+        $expectResult = 500;
+
+        $product = $this->getProduct($price);
+
+        $result = $this->pointService->getPoint($product);
+
+        $this->assertEquals($expectResult, $result);
+    }
+
+    /** @test */
+    public function 상품_가격의_5퍼센트가_실수인_경우_소수점을_버린_정수를_반환한다(): void
+    {
+        $price = 9750;
+        //가격이 9,750원일 경우 5%인 487.5에서 소수점을 버리고 487을 반환
+        $expectResult = 487;
+
+        $product = $this->getProduct($price);
+
+        $result = $this->pointService->getPoint($product);
+
+        $this->assertEquals($expectResult, $result);
     }
 }
