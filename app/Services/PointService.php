ea3c82cea953ceb8869150907781934943a0f1b2
sirwoongke <fzwoongbin@naver.com>
2022-02-20 16:21:28 +0900

    1-D 서비스 코드 작성 green 단계

--- app/Services/PointService.php
+++ app/Services/PointService.php
@@ -6,8 +6,10 @@ use App\Models\Product;
 
 class PointService
 {
+    public const POINT_RATE = 0.05;
+
     public function getPoint(Product $product)
     {
-        return 0;
+        return (int)floor($product->price * self::POINT_RATE);
     }
 }
