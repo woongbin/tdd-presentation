c65da4b0e24a4a31a9a4f516c8a75c7598aa24dd
sirwoongke <fzwoongbin@naver.com>
2022-02-25 08:32:31 +0900

    이벤트 작업 추가

--- app/Services/PointService.php
+++ app/Services/PointService.php
@@ -3,11 +3,19 @@
 namespace App\Services;
 
 use App\Models\Product;
+use Carbon\Carbon;
 
 class PointService
 {
-    public function getPoint(Product $product)
+    public function getPoint(Product $product, Carbon $date)
     {
-        return (int)floor($product->price * $product->pointRate);
+        $eventRate = $this->checkEventPeriod($date) === true ? 2 : 1;
+
+        return (int)floor($product->price * $product->pointRate) * $eventRate;
+    }
+
+    private function checkEventPeriod(Carbon $date): bool
+    {
+        return $date->year === 2022 && $date->month === 3;
     }
 }
