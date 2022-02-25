23ac17046de8a88eeb4476e2f435a4ebe71d68bf
sirwoongke <fzwoongbin@naver.com>
2022-02-25 09:44:22 +0900

    최대 포인트 적용

--- app/Services/PointService.php
+++ app/Services/PointService.php
@@ -7,11 +7,17 @@ use Carbon\Carbon;
 
 class PointService
 {
+    public const NORMAL_PERCENT_RATE = 1;
+    public const EVENT_PERCENT_RATE = 2;
+    public const MAX_POINT = 1000;
+
     public function getPoint(Product $product, Carbon $date)
     {
-        $eventRate = $this->checkEventPeriod($date) === true ? 2 : 1;
+        $eventRate = $this->checkEventPeriod($date) === true ? self::EVENT_PERCENT_RATE : self::NORMAL_PERCENT_RATE;
 
-        return (int)floor($product->price * $product->pointRate) * $eventRate;
+        $point = (int)floor($product->price * $product->pointRate) * $eventRate;
+
+        return min(self::MAX_POINT, $point);
     }
 
     private function checkEventPeriod(Carbon $date): bool
--- tests/Unit/PointServiceUnitTest.php
+++ tests/Unit/PointServiceUnitTest.php
@@ -72,13 +72,34 @@ class PointServiceUnitTest extends TestCase
     }
 
     /** @test */
-    public function 이벤트기간_2022년_3월에는_2배의_포인트를_제공한다(): void
+    public function 포인트는_최대_1000이다(): void
     {
         $testParameters = [];
-        $testParameters[] = ['price' => 10000, 'pointRate' => 0.1, 'result' => 2000];
+        $testParameters[] = ['price' => 50000, 'pointRate' => 0.1, 'result' => 1000];
+        $testParameters[] = ['price' => 10000, 'pointRate' => 0.2, 'result' => 1000];
+        $testParameters[] = ['price' => 10000, 'pointRate' => 0.05, 'result' => 500];
+        $testParameters[] = ['price' => 10000, 'pointRate' => 0.07, 'result' => 700];
+        $testParameters[] = ['price' => 8300, 'pointRate' => 0.1, 'result' => 830];
+        $testParameters[] = ['price' => 8375, 'pointRate' => 0.03, 'result' => 251];
+
+        $date = Carbon::create(2022, 2, 23, 5, 0, 0, 'Asia/Seoul');
+        foreach ($testParameters as $parameter) {
+            $product = $this->getProduct($parameter['price'], $parameter['pointRate']);
+
+            $result = $this->pointService->getPoint($product, $date);
+
+            $this->assertEquals($parameter['result'], $result);
+        }
+    }
+
+    /** @test */
+    public function 이벤트기간에도_포인트는_최대_1000이다(): void
+    {
+        $testParameters = [];
+        $testParameters[] = ['price' => 10000, 'pointRate' => 0.1, 'result' => 1000];
         $testParameters[] = ['price' => 10000, 'pointRate' => 0.05, 'result' => 1000];
-        $testParameters[] = ['price' => 10000, 'pointRate' => 0.07, 'result' => 1400];
-        $testParameters[] = ['price' => 8300, 'pointRate' => 0.1, 'result' => 1660];
+        $testParameters[] = ['price' => 10000, 'pointRate' => 0.07, 'result' => 1000];
+        $testParameters[] = ['price' => 8300, 'pointRate' => 0.1, 'result' => 1000];
         $testParameters[] = ['price' => 8375, 'pointRate' => 0.03, 'result' => 502];
 
         $date = Carbon::create(2022, 3, 15, 5, 0, 0, 'Asia/Seoul');
